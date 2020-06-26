2554759526cc43ffc5e90fcdd66b9d0e4f891028
Navnee <navnee.mundboth@mail.utoronto.ca>
2020-06-26 15:24:53 -0400

    Navnee - modified Admin

--- phase1/Admin.java
+++ phase1/Admin.java
@@ -1,9 +1,26 @@
 package group_0073.phase1;
 
+import java.util.List;
+
 public class Admin extends User{
 
+    private int notifs_count;
+
     public Admin(String username, String password, String type, Inbox inbox) {
+
         super(username, password, type, inbox);
+        this.notifs_count = 0;
+    }
+
+    public void readNotifs(){
+        this.notifs_count = 0;
+
+        //Reads notifications line by line
+        int i = 0;
+        while (i < this.getInbox().getNotifs().size()){
+            System.out.println(this.getInbox().getNotifs().get(i));
+            i++;
+        }
     }
 
 }
