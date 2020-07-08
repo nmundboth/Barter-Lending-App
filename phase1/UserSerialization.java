35028dfa5f8fc1fe1afedf7cc4a239fb6171d3d0
Xiao Tao <x.tao@mail.utoronto.ca>
2020-07-08 09:30:29 -0400

    Added a deserialize and serialize method to UserSerialization, which is the gateway class that manages User info.

---
+++ phase1/UserSerialization.java
@@ -0,0 +1,58 @@
+package phase1;
+
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class UserSerialization
+{
+    public static void toSerialize(ArrayList<User> users) throws Exception
+    {
+        ArrayList<User> userList = new ArrayList<User>(users);
+        try
+        {
+            FileOutputStream fos = new FileOutputStream("UserData");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(userList);
+            oos.close();
+            fos.close();
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("IO Error");
+        }
+    }
+
+    public static void deserialize() throws Exception
+    {
+        ArrayList<User> userList = new ArrayList<User>();
+
+        try
+        {
+            FileInputStream fis = new FileInputStream("UserData");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            userList = (ArrayList<User>) ois.readObject();
+
+            ois.close();
+            fis.close();
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("IO Error");
+        }
+        catch (ClassNotFoundException c)
+        {
+            System.out.println("Class not found");
+        }
+
+        //Verify list data
+        for (User name : userList) {
+            System.out.println(name);
+        }
+    }
+}
+
