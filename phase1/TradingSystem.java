a80aa54d0cbbc6853a7b82b5ecd8145a5d330d94
Ryan Wong <ryane.wong@mail.utoronto.ca>
2020-07-08 21:41:59 -0400

    Updated TradingSystem main class

--- phase1/TradingSystem.java
+++ phase1/TradingSystem.java
@@ -1,13 +1,36 @@
 package phase1;
 
 import UserSerialization;
+import User;
+import Inbox;
+import Trade;
 
 public class TradingSystem {
     public static void main(String[] args) {
-        userAccountOptions();
+        UserSerialization userSerialization = new UserSerialization();
+        Scanner userInfo = new Scanner(System.in);
+        System.out.println("Enter username")
+        String username = userInfo.nextLine();
+        System.out.println("Enter password")
+        String password = userInfo.nextLine();
+        System.out.println("1: Log in 2: Register");
+        int signInOption = userInfo.nextLine();
+
+        if (signInOption == 1) {
+            System.out.println("Account type: 'trader' or 'admin'");
+            int type = userInfo.nextLine();
+            ArrayList<Trade> trades = new ArrayList<Trade>();
+            ArrayList<String> strings = new ArrayList<String>();
+            Inbox inbox = new Inbox(trades, strings, strings);
+            User tempUser = new User(username, password, type, inbox);
+            userSerialization.userList.append();
+        }
+        else if (signInOption == 2) {
+            userSerialization.deSerialize();
+        }
     }
 
-    public static void userAccountOptions() {
+    /*public static void TradingSystem() {
         Scanner userInfo = new Scanner(System.in);
         System.out.println("Enter username")
         String username = userInfo.nextLine();
@@ -30,5 +53,5 @@ public class TradingSystem {
         else if (signInOption) == 2) {
             // add to list of accounts in userserialization
         }
-    }
+    }*/
 }
